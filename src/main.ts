703b0e25763090b16a550fa107566170d5b3b6dd
ArminFH <82161874+AFierroH@users.noreply.github.com>
2025-12-13 23:23:10 -0300

    Version 1.1.138 arreglo upload logo

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,25 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 1. Prefijo API (CRUCIAL)
+  // 1. ESTO FALTA O SE BORRÓ:
   app.setGlobalPrefix('api');
 
-  // 2. CORS (Permitir que el frontend se conecte)
   app.enableCors({
-    origin: '*', // Permitir todo por ahora para descartar problemas
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // 3. Aumentar tamaño de subida (para las imágenes)
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
 
-  // 4. Validaciones
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
   }));
 
-  // 5. ESCUCHAR EN 0.0.0.0 (CRUCIAL PARA DOCKER)
+  // 2. IMPORTANTE PARA DOCKER:
   await app.listen(3000, '0.0.0.0');
-  
   console.log(`Servidor corriendo en: ${await app.getUrl()}`);
 }
 bootstrap();
